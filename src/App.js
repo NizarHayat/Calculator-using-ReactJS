82765abb9519fee5d046f28e0b7def13c0c2f0a9
nizarhayat <nizarhayat351@gmail.com>
2023-03-07 16:55:47 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  }
+
+  const handleClear = () => {
+    setResult("");
+  }
+
+  const handleEqual = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className='h1'>Magic Calculator</h1>
+      <div className="display">
+        <input type="text" value={result} />
+      </div>
+
+      <div className="keypad">
+        <button onClick={handleClear} id="clear">Clear</button>
+
+        <button name="*" onClick={handleClick}>&times;</button>
+        <button name="/" onClick={handleClick}>&divide;</button>
+        <button name="-" onClick={handleClick}>-</button>
+        <button name="+" onClick={handleClick}>+</button>
+        <button name="" onClick={handleClick}>Mod</button>
+        
+        <button name="9" onClick={handleClick}>9</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="7" onClick={handleClick}>7</button>
+
+        <button name="6" onClick={handleClick}>6</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="4" onClick={handleClick}>4</button>
+
+        <button name="3" onClick={handleClick}>3</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="." onClick={handleClick}>.</button>
+        <button name="0" onClick={handleClick}>0</button>
+        
+        <button onClick={handleEqual} id="equal">=</button>
+      </div>
+      
+        
+    </div>
+  );
+}
+
+export default App;
